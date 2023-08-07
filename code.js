24d47e7b6000abb5dbe841ec76c8ab971b89a9b1
Donald Burr <dburr@borg-cube.com>
2023-08-07 01:14:03 -0700

    Re-organize data
    Games are defined separately, with the timezone info for each game
    there. (also added day reset time, though I'm not doing anything
    with that (yet.))
    
    Events are now a dictionary with (abbreviated) game name as the key.

--- code.js
+++ code.js
@@ -1,59 +1,96 @@
 var userTimezone = "America/Los_Angeles";       // a sensible default
-var events = [];
+var allGames = {};
+var allEvents = [];
 
 function getUserTimezone() {
   const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
   return tz;
 }
 
+function nameForGame(game) {
+  if (game in allGames) {
+    return allGames[game].name;
+  } else {
+    console.log("ERROR: unknown game `" + game + "'")
+    return null;
+  }
+}
+
+function timezoneForGame(game) {
+  if (game in allGames) {
+    return allGames[game].timezone;
+  } else {
+    console.log("ERROR: unknown game `" + game + "'")
+    return null;
+  }
+}
+
 function updateCountdown() {
-    if (events.length == 0) {
+    const keys = Object.keys(allEvents);
+
+    if (keys.length == 0) {
       console.log("No events found");
       // should probably put this in the html
       return;
     } else {
-      console.log("Found " + events.length + " events");
+      console.log("Found " + keys.length + " games");
     }
 
     const countdownContainer = $("#main");
     countdownContainer.empty();
     const now = moment.tz(moment(), userTimezone); // Replace with your current timezone
-
-    events.forEach(function (event) {
-      const startDate = moment.tz(event.start, event.timezone);
-      const endDate = moment.tz(event.end, event.timezone);
-
-      var tag = "";
-      var targetDate;
-      
-      if (now.isBefore(startDate, 'day')) {
-        console.log("event " + event.event + " is in THE FUTURE (zura)");
-        targetDate = startDate;
-        tag = "Starts";
-      } else if (now.isBefore(endDate, 'day')) {
-        console.log("event " + event.event + " is in the here and now");
-        targetDate = endDate;
-        tag = "Ends";
-      } else if (now.isAfter(endDate, 'day')) {
-        console.log("event " + event.event + " is but a distant memory");
-        return;
-      } else {
-        // this shouldn't happen...
-        console.log("oh no, you broke time again, didn't you?");
+    
+    keys.forEach(function (game) {
+      console.log(game);
+      const gameName = nameForGame(game);
+      if (gameName == null) {
+        console.log("could not look up game");
         return;
       }
-      
-      const duration = moment.duration(targetDate.diff(now));
-      const days = duration.days();
-      const hours = duration.hours();
-      const minutes = duration.minutes();
-      const seconds = duration.seconds();
 
-      const newDiv = $('<div>').addClass("countdown-timer");
-      newDiv.html('<h3>[' + event.game + '] ' + event.event + '</h3>');
-      newDiv.append(tag + " in ");
-      newDiv.append(`${days}d ${hours}h ${minutes}m ${seconds}s`);
-      countdownContainer.append(newDiv);
+      const eventTimezone = timezoneForGame(game);
+      allEvents[game].forEach(function (event) {
+        console.log(event);
+        if (eventTimezone == null) {
+          console.log("could not look up timezone");
+          return;
+        }
+      
+        const startDate = moment.tz(event.start, eventTimezone);
+        const endDate = moment.tz(event.end, eventTimezone);
+
+        var tag = "";
+        var targetDate;
+      
+        if (now.isBefore(startDate, 'day')) {
+          console.log("event " + event.event + " is in THE FUTURE (zura)");
+          targetDate = startDate;
+          tag = "Starts";
+        } else if (now.isBefore(endDate, 'day')) {
+          console.log("event " + event.event + " is in the here and now");
+          targetDate = endDate;
+          tag = "Ends";
+        } else if (now.isAfter(endDate, 'day')) {
+          console.log("event " + event.event + " is but a distant memory");
+          return;
+        } else {
+          // this shouldn't happen...
+          console.log("oh no, you broke time again, didn't you?");
+          return;
+        }
+      
+        const duration = moment.duration(targetDate.diff(now));
+        const days = duration.days();
+        const hours = duration.hours();
+        const minutes = duration.minutes();
+        const seconds = duration.seconds();
+
+        const newDiv = $('<div>').addClass("countdown-timer");
+        newDiv.html('<h3>[' + gameName + '] ' + event.event + '</h3>');
+        newDiv.append(tag + " in ");
+        newDiv.append(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+        countdownContainer.append(newDiv);
+      });
     });
 }
 
@@ -74,7 +111,8 @@ $(document).ready(function() {
       success: function(data) {
           console.log("Qapla!");
           console.log(data); // I don't see this message in console
-          events = data;
+          allGames = data.games;
+          allEvents = data.events;
       }
   });
   
