e598d79cdc3fba8ebd036db6a585afc6041ef440
Donald Burr <dburr@borg-cube.com>
2023-08-08 14:24:29 -0700

    Re-enable realtime updates

--- code.js
+++ code.js
@@ -3,9 +3,9 @@ var allGames = {};
 var allEvents = [];
 
 // only turn this on if you really like the idea of lots and lots of console spew
-const debug = true;
+const debug = false;
 // it's useful to turn this off while debugging, so you don't get spew every second
-const enableConstantUpdates = false;
+const enableConstantUpdates = true;
 
 function getUserTimezone() {
   const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
